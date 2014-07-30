78516ee82cafebe53c4213d066d8f8e706c39180
tonic <tonicbupt@gmail.com>
2014-07-30 11:47:29 +0800

    manage config

--- magneto/config.py
+++ magneto/config.py
@@ -1,6 +1,16 @@
 # coding: utf-8
 
 DEV = False
+mysql_config = {
+    'user': 'platform_root',
+    'passwd': 'mghqFGeksh342ui',
+    'host': '10.1.201.58',
+    'database': 'sri',
+}
+database_uri = 'mysql://{user}:{passwd}@{host}/{database}'.format(**mysql_config)
+
+redis_host = '10.1.201.16'
+redis_port = 6379
 
 try:
     from local_config import *
--- magneto/libs/store.py
+++ magneto/libs/store.py
@@ -7,10 +7,11 @@ from sqlalchemy.orm.scoping import scoped_session
 import redis
 
 from .queue import RedisBlockQueue
+from magneto.config import database_uri, redis_host, redis_port
 
-engine = create_engine('sqlite:///magneto.db')
+engine = create_engine(database_uri)
 session = scoped_session(sessionmaker(bind=engine))
 
-rds = redis.Redis()
+rds = redis.Redis(host=redis_host, port=redis_port)
 taskqueue = RedisBlockQueue('taskqueue', 15, redis_instance=rds)
 tasklock = rds.lock('magneto:redis:tasklock', timeout=120, sleep=5)
